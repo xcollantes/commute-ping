6d9a9ba8b4b9af95cbff5dbe31e71c7004b74912
Xavier Collantes <xcollantes@google.com>
2020-01-24 21:41:42 -0800

    Add subprocess lib

---
+++ Notifier.py
@@ -0,0 +1,18 @@
+"""Desktop notification system for commuters.
+
+TODO(xcollantes): DO NOT SUBMIT without a detailed description of Notifier.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import subprocess
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+  main()
